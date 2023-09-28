0fe14c20e7010639a7a6876c80d15c057cc45bdc
Roustam <roustam.ts@gmail.com>
2023-09-28 22:25:59 +0400

    next config setup images and assets

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // output: 'export',
+    output: 'export',
+    assetPrefix: '/out',
+    images: {
+        unoptimized: true,
+      },
 }
 
 module.exports = nextConfig
