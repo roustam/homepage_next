9c9883e7a7cafff517ab5cda8f52cb91f2b9d63b
Roustam <roustam.ts@gmail.com>
2023-09-29 14:38:18 +0400

    fixed header on contacts page

--- src/app/contacts/page.tsx
+++ src/app/contacts/page.tsx
@@ -2,7 +2,7 @@ import TopNavbar from "../components/TopNavbar";
 
 export default function Contacts() {
     return (
-        <main>
+        <main className="flex min-h-screen w-2/3 flex-col items-center mx-auto">
             <TopNavbar />
             <h1 className="flex min-h-screen w-2/3 flex-col items-center mx-auto">My contacts</h1>
         </main>
